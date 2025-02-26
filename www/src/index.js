3d0a76c4b00cff01ad06944abb130ebe2d52df6c
maxim-polyakov <maxim7012@mail.ru>
2025-02-27 02:31:12 +0300

    1

--- www/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import * as ReactDomClient from "react-dom/client";
-import App from "./App";
-import './css/main.css';
-
-const app = ReactDomClient.createRoot(document.getElementById("app"));
-app.render(<App />)
