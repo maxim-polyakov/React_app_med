dfb9142ce30f8cc134d8c8f17e05000f81f16527
maxim-polyakov <maxim7012@mail.ru>
2025-02-27 02:14:10 +0300

    1

--- www/src/App.js
+++ www/src/App.js
@@ -4,19 +4,36 @@ import Image from "./components/Image";
 import logo from "./img/logo.png"
 
 class App extends React.Component{
-    helpText = "Help text!"
+    constructor(props) {
+        super(props);
+        this.state = {
+            helpText: "Help text",
+            userData: ""
+        }
+        this.inputClick = this.inputClick.bind(this);
+    }
+    componentDidUpdate(prevProp) {
+        if(this.state.helpText !== "Help")
+            console.log("Some")
+    }
+
     render(){
         return(<div className="name">
             <Header />
-            <h1>{this.helpText}</h1>
-            <input placeholder={this.helpText}
+            <h1>{this.state.helpText}</h1>
+            <h2>{this.state.userData}</h2>
+            <input placeholder={this.state.helpText}
+                   onChange={event => this.setState({userData: event.target.value})}
                    onClick={this.inputClick} onMouseEnter={this.mouseOver} />
-            <p>{this.helpText==="Help text!" ? "Yes" : "No"}</p>
+            <p>{this.state.helpText==="Help text!" ? "Yes" : "No"}</p>
             <Image image={logo} />
             <img src={logo} />
         </div>)
     }
-    inputClick(){ console.log("Clicked!");}
+    inputClick(){
+        this.setState({helpText: "Changed"});
+        console.log("Clicked!");
+    }
     mouseOver(){console.log("mouseOver!");}
 }
 export default App;
--- www/src/components/Header.js
+++ www/src/components/Header.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Button from "./Button";
 
 class Header extends React.Component{
     render() {
-        return (<header className="header">Шапка сайта</header>)
+        return (<header className="header">
+                                            {this.props.title}
+                                            <Button text="Button" /></header>)
     }
 }
 export default Header;
